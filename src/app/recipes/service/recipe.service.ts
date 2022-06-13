acb50b4f9f7c3bab89007e0507f00ec28b84e9c1
AntoninB4440 <barceloantonin@gmail.com>
2022-06-13 15:31:30 +0200

    Adding Ingredient toshoppingList logic from recipe

--- src/app/recipes/service/recipe.service.ts
+++ src/app/recipes/service/recipe.service.ts
@@ -1,23 +1,34 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from 'src/model/ingredient.model';
 import { Recipe } from 'src/model/recipe.model';
+import { ShoppingListService } from '../../shopping-list/service/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://static01.nyt.com/images/2021/03/28/dining/mc-shakshuka/mc-shakshuka-articleLarge.jpg'
+      'https://static01.nyt.com/images/2021/03/28/dining/mc-shakshuka/mc-shakshuka-articleLarge.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'An other Test Recipe',
       'This is simply a test',
-      'https://static01.nyt.com/images/2021/03/28/dining/mc-shakshuka/mc-shakshuka-articleLarge.jpg'
+      'https://static01.nyt.com/images/2021/03/28/dining/mc-shakshuka/mc-shakshuka-articleLarge.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   public recipeSelected = new EventEmitter<Recipe>();
 
   public getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
